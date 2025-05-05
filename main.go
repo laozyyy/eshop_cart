0926b311a99db8eb8997b4d4edb3dcd9a65930b4
zhongyiming.laozyi <zhongyiming.laozyi@dcarlife.com>
2025-05-05 16:52:07 +0800

    feat: 注册中心

--- main.go
+++ main.go
@@ -5,15 +5,22 @@ import (
 	"eshop_cart/kitex_gen/eshop/cart/cartservice"
 	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
+	zkregistry "github.com/kitex-contrib/registry-zookeeper/registry"
+	"time"
 )
 
 func main() {
 	database.Init()
-	svr := cartservice.NewServer(
-		new(CartServiceImpl),
-		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: "CartService"}),
-	)
-	err := svr.Run()
+	r, err := zkregistry.NewZookeeperRegistry([]string{"117.72.72.114:2181"}, 40*time.Second)
+	if err != nil {
+		panic(err)
+	}
+	svr := cartservice.NewServer(new(CartServiceImpl), server.WithRegistry(r), server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: "eshop_cart"}))
+	//svr := cartservice.NewServer(
+	//	new(CartServiceImpl),
+	//	server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: "CartService"}),
+	//)
+	err = svr.Run()
 	if err != nil {
 		panic(err)
 	}
